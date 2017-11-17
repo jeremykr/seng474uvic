83d3f127c74a0e7994ba9927d9fcb3c1f1fd7fcb
Carl Marais <10454667+carlmarais@users.noreply.github.com>
2017-11-16 21:19:39 -0800

    Updated main.py
    Some clean-up. Nifty stats added.

--- Classification/main.py
+++ Classification/main.py
@@ -8,93 +8,88 @@ from sklearn.linear_model import BayesianRidge, LogisticRegression, SGDRegressor
 
 # Train and test using the given classifier with k-fold cross-validation
 def classify(X, y, k, clf):
-    # Split data into folds
-    kf = KFold(n_splits=k, shuffle=True)
-    splits = kf.split(X)
+	# Split data into folds
+	kf = KFold(n_splits=k, shuffle=True)
+	splits = kf.split(X)
+	
+	abs_err = 0
+	per_err = 0
+	count = 0
 
-    err = 0
-    for train_index, test_index in splits:
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
-        err += mean_absolute_error(y_test, y_pred)
-    print(err/k)
+	for train_index, test_index in splits:
+		X_train, X_test = X[train_index], X[test_index]
+		y_train, y_test = y[train_index], y[test_index]
+		clf.fit(X_train, y_train)
+		y_pred = clf.predict(X_test)
+
+		abs_err += mean_absolute_error(y_test, y_pred)
+		per_err += np.mean(np.abs((y_test - y_pred) / y_test) * 100)
+
+	#Some statistics
+	for i in range(len(y_pred)):
+		if np.mean(np.abs((y_test[i]-y_pred[i]))/y_test[i])*100 >= 20:
+			'''print("True price: " + str(round(y_test[i], 2)) + 
+					 "\nEst. price: " + str(round(y_pred[i], 2)) +
+					 "\nAbsolute error: " + str(round(np.abs(y_test[i]-y_pred[i]), 2)) +
+					 "\nPercentage error: " + str(round(np.mean(np.abs((y_test[i]-y_pred[i]))/y_test[i])*100, 2)) +
+					 "\nBuilding type: " + str(round(X_test[i][6], 2)) + "\n")
+			'''
+			count += 1
+
+	print("===========================================================")
+	print("Number of  properties with >= 20% error: " + str(count))
+	print("Mean absolute percentage error: " + str(round(per_err/k, 2)))
+	print("Mean absolute error: " + str(round(abs_err/k, 2)))
+	print("")
 
 def main():
-    houses = Utils.get_house_data('../RemaxScrape/remaxDataset2.json')
-    print("Total listings: " + str(len(houses)))
-    X, y = Utils.create_matrices(houses, 8)
+	houses = Utils.get_house_data('../RemaxScrape/remaxDataset2.json')
+	print("Total listings: " + str(len(houses)) + "\n")
+	X, y = Utils.create_matrices(houses, 7)
 
-    # Scale feature data a bit (doesn't seem to help much)
-    X = preprocessing.scale(X) 
+	# Scale feature data a bit (doesn't seem to help much)
+	X = preprocessing.scale(X) 
 
-    n_splits = 5
-    
-    print("Support Vector Regression with " + str(n_splits) + "-fold cross-validation")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, svm.SVR())
-    print()  
+	n_splits = 5
+	
+	print("Support Vector Regression with " + str(n_splits) + "-fold cross-validation")
+	classify(X, y, n_splits, svm.SVR())  
 
-    print("Bayesian Ridge Regression with " + str(n_splits) + "-fold cross-validation")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, BayesianRidge())
-    print()
+	print("Bayesian Ridge Regression with " + str(n_splits) + "-fold cross-validation")
+	classify(X, y, n_splits, BayesianRidge())
 
-    print("Logistic Regression with " + str(n_splits) + "-fold cross-validation, liblinear solver")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, LogisticRegression(solver="liblinear"))
-    print()
+	print("Logistic Regression with " + str(n_splits) + "-fold cross-validation, liblinear solver")
+	classify(X, y, n_splits, LogisticRegression(solver="liblinear"))
+	
+	print("Logistic Regression with " + str(n_splits) + "-fold cross-validation, newton-cg solver")
+	classify(X, y, n_splits, LogisticRegression(solver="newton-cg"))
+	
+	print("Logistic Regression with " + str(n_splits) + "-fold cross-validation, lbfgs solver")
+	classify(X, y, n_splits, LogisticRegression(solver="lbfgs"))
+	
+	print("Stochastic Gradient Descent Regressor with " + str(n_splits) + "-fold cross-validation, squared loss")
+	classify(X, y, n_splits, SGDRegressor(loss="squared_loss"))
 
-    print("Logistic Regression with " + str(n_splits) + "-fold cross-validation, newton-cg solver")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, LogisticRegression(solver="newton-cg"))
-    print()
+	print("Stochastic Gradient Descent Regressor with " + str(n_splits) + "-fold cross-validation, huber loss")
+	classify(X, y, n_splits, SGDRegressor(loss="huber"))
+	
+	print("Stochastic Gradient Descent Regressor with " + str(n_splits) + "-fold cross-validation, epsilon insensitive loss")
+	classify(X, y, n_splits, SGDRegressor(loss="epsilon_insensitive"))
 
-    print("Logistic Regression with " + str(n_splits) + "-fold cross-validation, lbfgs solver")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, LogisticRegression(solver="lbfgs"))
-    print()
-
-    print("Stochastic Gradient Descent Regressor with " + str(n_splits) + "-fold cross-validation, squared loss")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, SGDRegressor(loss="squared_loss"))
-    print()
-
-    print("Stochastic Gradient Descent Regressor with " + str(n_splits) + "-fold cross-validation, huber loss")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, SGDRegressor(loss="huber"))
-    print()
-    
-    print("Stochastic Gradient Descent Regressor with " + str(n_splits) + "-fold cross-validation, epsilon insensitive loss")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, SGDRegressor(loss="epsilon_insensitive"))
-    print()
-
-    print("Stochastic Gradient Descent Regressor with " + str(n_splits) + "-fold cross-validation, squared epsilon insensitive loss")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, SGDRegressor(loss="squared_epsilon_insensitive"))
-    print()
-    
-    print("Perceptron with " + str(n_splits) + "-fold cross-validation")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, Perceptron())
-    print()
-    
-    print("Passive-Aggressive Regressor with " + str(n_splits) + "-fold cross-validation")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, PassiveAggressiveRegressor())
-    print()
-    
-    print("RANSAC Regressor with " + str(n_splits) + "-fold cross-validation")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, RANSACRegressor())
-    print()
-    
-    print("Theil-Sen Regressor with " + str(n_splits) + "-fold cross-validation")
-    print("==========mean absolute error==========")
-    classify(X, y, 5, TheilSenRegressor())
-    print()
+	print("Stochastic Gradient Descent Regressor with " + str(n_splits) + "-fold cross-validation, squared epsilon insensitive loss")
+	classify(X, y, n_splits, SGDRegressor(loss="squared_epsilon_insensitive"))
+	
+	print("Perceptron with " + str(n_splits) + "-fold cross-validation")
+	classify(X, y, n_splits, Perceptron())
+	
+	print("Passive-Aggressive Regressor with " + str(n_splits) + "-fold cross-validation")
+	classify(X, y, n_splits, PassiveAggressiveRegressor())
+	
+	print("RANSAC Regressor with " + str(n_splits) + "-fold cross-validation")
+	classify(X, y, n_splits, RANSACRegressor())
+	
+	print("Theil-Sen Regressor with " + str(n_splits) + "-fold cross-validation")
+	classify(X, y, n_splits, TheilSenRegressor())
 
 if __name__ == "__main__":
-    main()
+	main()
